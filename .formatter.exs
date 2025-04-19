b69738f74300a7f5f0cd81d3a05010bf53a108ab
Ramon de Lima Ramos <ramonlimaramos@gmail.com>
2025-04-18 21:49:40 -0300

    feat: adds formatter file

---
+++ .formatter.exs
@@ -0,0 +1,10 @@
+# Used by "mix format"
+[
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 98,
+  import_deps: [],
+  plugins: [],
+  export: [
+    locals_without_parens: []
+  ]
+]
